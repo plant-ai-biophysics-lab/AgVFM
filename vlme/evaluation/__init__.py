6f6c44013cace2e07f808a84ead9c6435f15cf53
llund <llund@ucdavis.edu>
2026-03-20 14:20:14 -0700

    Removed old vlme

--- vlme/evaluation/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Evaluation: metrics (mAP@0.5, P, R) and run_evaluation pipeline."""
-
-from vlme.evaluation.metrics import compute_metrics_at_iou
-from vlme.evaluation.run_eval import run_evaluation
-
-__all__ = ["compute_metrics_at_iou", "run_evaluation"]
--- vlme/viz/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Visualization utilities."""
-
-from vlme.viz.boxes import plot_gt_vs_predictions, DEFAULT_PREDICT_KWARGS
-from vlme.viz.eval_plots import plot_eval_summary
-
-__all__ = ["plot_gt_vs_predictions", "DEFAULT_PREDICT_KWARGS", "plot_eval_summary"]
